a866a1a280b20b8016cb70252ddb751d2d53b2bf
Satyanarayan Dalei <satya.narayan@quotus.co.in>
2024-09-12 12:28:10 +0530

    Changes redis config

--- openai/src/config/kue.ts
+++ openai/src/config/kue.ts
@@ -1,10 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
 import kue from "kue";
+
+const redisHost = process.env.REDIS_HOST;
 
 const queue = kue.createQueue({
   prefix: "queue",
   redis: {
-    host:  "redis", 
-    // "redis", //redis
+    host:  redisHost, 
+    // "redis", //127.0.0.1
     port: 6379,
   },
 });
